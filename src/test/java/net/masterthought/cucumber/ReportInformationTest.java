0d58d464aeb1657de5655293d552ede7751c25c8
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2012-07-06 23:13:03 +0100

    added more tests to reportinformation

--- src/test/java/net/masterthought/cucumber/ReportInformationTest.java
+++ src/test/java/net/masterthought/cucumber/ReportInformationTest.java
@@ -18,13 +18,13 @@ public class ReportInformationTest {
 
     @Before
     public void setUpReportInformation() throws IOException {
-    ConfigurationOptions.setSkippedFailsBuild(false);
-    ConfigurationOptions.setUndefinedFailsBuild(false);
-    List<String> jsonReports = new ArrayList<String>();
-    jsonReports.add("src/test/resources/net/masterthought/cucumber/project1.json");
-    jsonReports.add("src/test/resources/net/masterthought/cucumber/project2.json");
-    reportParser = new ReportParser(jsonReports);
-    reportInformation = new ReportInformation(reportParser.getFeatures());
+        ConfigurationOptions.setSkippedFailsBuild(false);
+        ConfigurationOptions.setUndefinedFailsBuild(false);
+        List<String> jsonReports = new ArrayList<String>();
+        jsonReports.add("src/test/resources/net/masterthought/cucumber/project1.json");
+        jsonReports.add("src/test/resources/net/masterthought/cucumber/project2.json");
+        reportParser = new ReportParser(jsonReports);
+        reportInformation = new ReportInformation(reportParser.getFeatures());
     }
 
     @Test
@@ -33,119 +33,129 @@ public class ReportInformationTest {
     }
 
     @Test
-    public void shouldListAllTags(){
-      assertThat(reportInformation.getTags().get(0),is(TagObject.class));
+    public void shouldListAllTags() {
+        assertThat(reportInformation.getTags().get(0), is(TagObject.class));
     }
 
     @Test
-    public void shouldListFeaturesInAMap(){
-        assertThat(reportInformation.getProjectFeatureMap().keySet().iterator().next(),is("src/test/resources/net/masterthought/cucumber/project2.json"));
-        assertThat(reportInformation.getProjectFeatureMap().entrySet().iterator().next().getValue().get(0),is(Feature.class));
+    public void shouldListFeaturesInAMap() {
+        assertThat(reportInformation.getProjectFeatureMap().keySet().iterator().next(), is("src/test/resources/net/masterthought/cucumber/project2.json"));
+        assertThat(reportInformation.getProjectFeatureMap().entrySet().iterator().next().getValue().get(0), is(Feature.class));
     }
 
     @Test
-    public void shouldReturnTotalNumberOfScenarios(){
-        assertThat(reportInformation.getTotalNumberOfScenarios(),is(18));
+    public void shouldReturnTotalNumberOfScenarios() {
+        assertThat(reportInformation.getTotalNumberOfScenarios(), is(18));
     }
 
     @Test
-    public void shouldReturnTotalNumberOfFeatures(){
-        assertThat(reportInformation.getTotalNumberOfFeatures(),is(4));
+    public void shouldReturnTotalNumberOfFeatures() {
+        assertThat(reportInformation.getTotalNumberOfFeatures(), is(4));
     }
 
     @Test
-    public void shouldReturnTotalNumberOfSteps(){
-        assertThat(reportInformation.getTotalNumberOfSteps(),is(98));
+    public void shouldReturnTotalNumberOfSteps() {
+        assertThat(reportInformation.getTotalNumberOfSteps(), is(98));
     }
 
     @Test
-    public void shouldReturnTotalNumberPassingSteps(){
-        assertThat(reportInformation.getTotalNumberPassingSteps(),is(90));
+    public void shouldReturnTotalNumberPassingSteps() {
+        assertThat(reportInformation.getTotalNumberPassingSteps(), is(90));
     }
 
     @Test
-    public void shouldReturnTotalNumberFailingSteps(){
-        assertThat(reportInformation.getTotalNumberFailingSteps(),is(2));
+    public void shouldReturnTotalNumberFailingSteps() {
+        assertThat(reportInformation.getTotalNumberFailingSteps(), is(2));
     }
 
     @Test
     public void shouldReturnTotalNumberSkippedSteps() {
-        assertThat(reportInformation.getTotalNumberSkippedSteps(),is(6));
+        assertThat(reportInformation.getTotalNumberSkippedSteps(), is(6));
     }
 
     @Test
     public void shouldReturnTotalNumberPendingSteps() {
-        assertThat(reportInformation.getTotalNumberPendingSteps(),is(0));
+        assertThat(reportInformation.getTotalNumberPendingSteps(), is(0));
     }
 
     @Test
     public void shouldReturnTotalNumberMissingSteps() {
-        assertThat(reportInformation.getTotalNumberMissingSteps(),is(0));
+        assertThat(reportInformation.getTotalNumberMissingSteps(), is(0));
     }
 
     @Test
     public void shouldReturnTotalDuration() {
-        assertThat(reportInformation.getTotalDuration(),is(236050000L));
+        assertThat(reportInformation.getTotalDuration(), is(236050000L));
     }
 
     @Test
     public void shouldReturnTotalDurationAsString() {
-        assertThat(reportInformation.getTotalDurationAsString(),is("236 ms"));
+        assertThat(reportInformation.getTotalDurationAsString(), is("236 ms"));
     }
 
     @Test
     public void shouldReturnTimeStamp() {
-        assertThat(reportInformation.timeStamp(),is(String.class));
+        assertThat(reportInformation.timeStamp(), is(String.class));
     }
 
     @Test
     public void shouldReturnReportStatusColour() {
-        assertThat(reportInformation.getReportStatusColour(reportInformation.getFeatures().get(0)),is("#C5D88A"));
+        assertThat(reportInformation.getReportStatusColour(reportInformation.getFeatures().get(0)), is("#C5D88A"));
     }
 
     @Test
     public void shouldReturnTagReportStatusColour() {
-        assertThat(reportInformation.getTagReportStatusColour(reportInformation.tagMap.get(0)),is("#C5D88A"));
+        assertThat(reportInformation.getTagReportStatusColour(reportInformation.tagMap.get(0)), is("#C5D88A"));
     }
 
     @Test
     public void shouldReturnTotalTags() {
-        assertThat(reportInformation.getTotalTags(),is(3));
+        assertThat(reportInformation.getTotalTags(), is(3));
     }
 
     @Test
     public void shouldReturnTotalTagScenarios() {
-        assertThat(reportInformation.getTotalTagScenarios(),is(24));
+        assertThat(reportInformation.getTotalTagScenarios(), is(24));
     }
 
     @Test
     public void shouldReturnTotalTagSteps() {
-        assertThat(reportInformation.getTotalTagSteps(),is(696));
+        assertThat(reportInformation.getTotalTagSteps(), is(696));
     }
 
     @Test
     public void shouldReturnTotalTagPasses() {
-        assertThat(reportInformation.getTotalTagPasses(),is(120));
+        assertThat(reportInformation.getTotalTagPasses(), is(120));
     }
 
     @Test
     public void shouldReturnTotalTagFails() {
-        assertThat(reportInformation.getTotalTagFails(),is(0));
+        assertThat(reportInformation.getTotalTagFails(), is(0));
     }
 
     @Test
     public void shouldReturnTotalTagSkipped() {
-        assertThat(reportInformation.getTotalTagSkipped(),is(0));
+        assertThat(reportInformation.getTotalTagSkipped(), is(0));
     }
 
     @Test
     public void shouldReturnTotalTagPending() {
-        assertThat(reportInformation.getTotalTagPending(),is(0));
+        assertThat(reportInformation.getTotalTagPending(), is(0));
     }
 
     @Test
     public void shouldReturnTotalTagDuration() {
-        assertThat(reportInformation.getTotalTagDuration(),is("340 ms"));
+        assertThat(reportInformation.getTotalTagDuration(), is("340 ms"));
+    }
+
+    @Test
+    public void shouldReturnTotalScenariosPassed() {
+        assertThat(reportInformation.getTotalScenariosPassed(), is(16));
+    }
+
+    @Test
+    public void shouldReturnTotalScenariosFailed() {
+        assertThat(reportInformation.getTotalScenariosFailed(), is(2));
     }
 
 
