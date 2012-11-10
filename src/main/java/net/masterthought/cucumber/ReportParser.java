fc14da2b148a7dacb3f51b47ee39c1578e3f4eb1
jan-molak <jan.molak@smartcodeltd.co.uk>
2012-11-10 20:45:35 +0000

    Fixed formatting

--- src/main/java/net/masterthought/cucumber/ReportParser.java
+++ src/main/java/net/masterthought/cucumber/ReportParser.java
@@ -15,23 +15,23 @@ public class ReportParser {
     private Map<String, List<Feature>> jsonReportFiles;
 
     public ReportParser(List<String> jsonReportFiles) throws IOException {
-      this.jsonReportFiles = parseJsonResults(jsonReportFiles);
+        this.jsonReportFiles = parseJsonResults(jsonReportFiles);
     }
 
-    public Map<String, List<Feature>> getFeatures(){
-      return jsonReportFiles;
+    public Map<String, List<Feature>> getFeatures() {
+        return jsonReportFiles;
     }
 
     private Map<String, List<Feature>> parseJsonResults(List<String> jsonReportFiles) throws IOException {
-           Map<String, List<Feature>> featureResults = new LinkedHashMap<String, List<Feature>>();
-           for (String jsonFile : jsonReportFiles) {
-               String fileContent = Util.readFileAsString(jsonFile);
-               if(Util.isValidCucumberJsonReport(fileContent)){
-               Feature[] features = new Gson().fromJson(Util.U2U(fileContent), Feature[].class);
-               featureResults.put(jsonFile, Arrays.asList(features));
-               }
-           }
-           return featureResults;
-       }
+        Map<String, List<Feature>> featureResults = new LinkedHashMap<String, List<Feature>>();
+        for (String jsonFile : jsonReportFiles) {
+            String fileContent = Util.readFileAsString(jsonFile);
+            if (Util.isValidCucumberJsonReport(fileContent)) {
+                Feature[] features = new Gson().fromJson(Util.U2U(fileContent), Feature[].class);
+                featureResults.put(jsonFile, Arrays.asList(features));
+            }
+        }
 
+        return featureResults;
+    }
 }
