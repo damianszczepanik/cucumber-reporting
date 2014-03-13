damianszczepanik/cucumber-reporting

commit 906b9b92a12c5e4c8512b466ca725d2cb74e2653

added support for step duration

src/main/java/net/masterthought/cucumber/json/Step.java
@@ -136,12 +136,12 @@ public class Step {
             if (getInternalStatus() == Util.Status.UNDEFINED) {
                 errorMessage = "Mode: Not Implemented causes Failure<br/><span class=\"undefined\">This step is not yet implemented</span>";
             }
-            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span>" + "<div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv() + getImageTags();
+            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span><span class=\"step-duration\">" + Util.formatDuration(result.getDuration()) + "</span><div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv() + getImageTags();
         } else if (getStatus() == Util.Status.MISSING) {
             String errorMessage = "<span class=\"missing\">Result was missing for this step</span>";
-            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span>" + "<div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv();
+            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span><span class=\"step-duration\">" + Util.formatDuration(result.getDuration()) + "</span><div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv();
         } else {
-            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span>" + Util.closeDiv() + getImageTags();
+            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span><span class=\"step-duration\">" + Util.formatDuration(result.getDuration()) + "</span>" + Util.closeDiv() + getImageTags();
         }
         return content;
     }

src/main/resources/templates/featureReport.vm
@@ -63,6 +63,7 @@
 .scenario-scenario_name{padding-left:15px;}
 .step-keyword{font-weight:bold;padding-left:50px;}
 .step-error-message{background-color:#FFEEEE;padding-left:50px;border: 1px solid #D88A8A;}
+.step-duration{background-color:gold;float:right;padding-right:20px;}
 .passed{background-color:#C5D88A;}
 .failed{background-color:#D88A8A;}
 .skipped{background-color:#2DEAEC;}

src/test/java/net/masterthought/cucumber/ReportBuilderTest.java
@@ -3,11 +3,12 @@ package net.masterthought.cucumber;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -61,6 +62,7 @@ public class ReportBuilderTest {
         assertThat(fromId("feature-title", doc).text(), is("Feature Result for Build: 1"));
         assertStatsHeader(doc);
         assertStatsFirstFeature(doc);
+        assertFeatureContent(doc);
     }
 
     @Test
@@ -116,7 +118,75 @@ public class ReportBuilderTest {
         assertThat("stats-number-steps-failed", fromId("stats-number-steps-failed-Account Holder withdraws cash", doc).text(), is("0"));
         assertThat("stats-number-steps-skipped", fromId("stats-number-steps-skipped-Account Holder withdraws cash", doc).text(), is("0"));
         assertThat("stats-number-steps-pending", fromId("stats-number-steps-pending-Account Holder withdraws cash", doc).text(), is("0"));
+        assertThat("stats-duration", fromId("stats-duration-Account Holder withdraws cash", doc).text(), is("112 ms"));
         assertNotNull(fromId("stats-duration-Account Holder withdraws cash", doc));
+    }
+
+    private void assertFeatureContent(Document doc) {
+        Elements elements = doc.select("div.passed");
+
+        assertThat("feature-keyword", elements.select("div.feature-line span.feature-keyword").first().text(), is("Feature:"));
+        assertThat("feature-text", elements.select("div.feature-line").first().text(), is("Feature: Account Holder withdraws cash"));
+        assertThat("feature-description", doc.select("div.feature-description").first().text(),
+                is("As a Account Holder I want to withdraw cash from an ATM So that I can get money when the bank is closed"));
+        assertThat("scenario-background-keyword", doc.select("div.passed span.scenario-keyword").first().text(), is("Background:"));
+        assertThat("scenario-background-name", doc.select("div.passed span.scenario-name").first().text(), is("Activate Credit Card"));
+
+        elements = doc.select("div.passed span.step-keyword");
+        List<String> backgroundStepKeywords = new ArrayList<String>();
+        List<String> firstScenarioStepKeywords = new ArrayList<String>();
+        for (Element element : elements) {
+            int index = elements.indexOf(element);
+            if (index < 3) {
+                backgroundStepKeywords.add(element.text());
+            } else if (index >= 3 && index < 10) {
+                firstScenarioStepKeywords.add(element.text());
+            } else {
+                break;
+            }
+        }
+        assertThat("Background step keywords must be same", backgroundStepKeywords, is(Arrays.asList(new String[] {"Given", "When", "Then"})));
+        assertThat("First scenario step keywords must be same", firstScenarioStepKeywords,
+                is(Arrays.asList(new String[] {"Given", "And", "And", "When", "Then", "And", "And"})));
+
+        elements = doc.select("div.passed span.step-name");
+        List<String> backgroundStepNames = new ArrayList<String>();
+        List<String> firstScenarioStepNames = new ArrayList<String>();
+        for (Element element : elements) {
+            int index = elements.indexOf(element);
+            if (index < 3) {
+                backgroundStepNames.add(element.text());
+            } else if (index >= 3 && index < 10) {
+                firstScenarioStepNames.add(element.text());
+            } else {
+                break;
+            }
+        }
+        assertThat("Background step names must be same", backgroundStepNames,
+                is(Arrays.asList(new String[] {
+                        "I have a new credit card",
+                        "I confirm my pin number",
+                        "the card should be activated"})));
+        assertThat("First scenario step names must be same", firstScenarioStepNames,
+                is(Arrays.asList(new String[] {
+                        "the account balance is 100",
+                        "the card is valid",
+                        "the machine contains 100",
+                        "the Account Holder requests 10",
+                        "the ATM should dispense 10",
+                        "the account balance should be 90",
+                        "the card should be returned"})));
+
+        elements = doc.select("div.passed span.step-duration");
+        assertFalse("step durations must not be empty", elements.isEmpty());
+        List<String> stepDurations = new ArrayList<String>();
+        for (Element element : elements) {
+            stepDurations.add(element.text());
+            int index = elements.indexOf(element);
+            if (index >= 10) break;
+        }
+        assertThat("Step durations must be same", stepDurations,
+                is(Arrays.asList(new String [] {"107 ms", "0 ms", "0 ms", "0 ms", "0 ms", "0 ms", "0 ms", "3 ms", "0 ms", "0 ms", "0 ms"})));
     }
 
     private void assertStatsTotals(Document doc) {

src/test/java/net/masterthought/cucumber/ReportInformationTest.java
@@ -42,7 +42,7 @@ public class ReportInformationTest {
         Map<String, Artifact> map = artifactProcessor.process();
         ConfigurationOptions.setArtifactConfiguration(map);
         reportInformation = new ReportInformation(reportParser.getFeatures());
-        assertThat(reportInformation.getFeatures().get(2).getElements().get(7).getSteps().get(0).getName(), is("<div class=\"passed\"><span class=\"step-keyword\">Given  </span><span class=\"step-name\">the account <div style=\"display:none;\"><textarea id=\"Account_has_sufficient_funds_againthe_account_balance_is_300\" class=\"brush: xml;\"></textarea></div><a onclick=\"applyArtifact('Account_has_sufficient_funds_againthe_account_balance_is_300','account_balance.txt')\" href=\"#\">balance</a> is 300</span></div>"));
+        assertThat(reportInformation.getFeatures().get(2).getElements().get(7).getSteps().get(0).getName(), is("<div class=\"passed\"><span class=\"step-keyword\">Given  </span><span class=\"step-name\">the account <div style=\"display:none;\"><textarea id=\"Account_has_sufficient_funds_againthe_account_balance_is_300\" class=\"brush: xml;\"></textarea></div><a onclick=\"applyArtifact('Account_has_sufficient_funds_againthe_account_balance_is_300','account_balance.txt')\" href=\"#\">balance</a> is 300</span><span class=\"step-duration\">0 ms</span></div>"));
     }
 
     @Test

src/test/java/net/masterthought/cucumber/StepTest.java
@@ -80,21 +80,21 @@ public class StepTest {
 
     @Test
     public void shouldReturnName() {
-        assertThat(passingStep.getName(), is("<div class=\"passed\"><span class=\"step-keyword\">Given  </span><span class=\"step-name\">I have a new credit card</span></div>"
+        assertThat(passingStep.getName(), is("<div class=\"passed\"><span class=\"step-keyword\">Given  </span><span class=\"step-name\">I have a new credit card</span><span class=\"step-duration\">107 ms</span></div>"
         ));
     }
 
     @Test
     public void shouldReturnNameWhenStepSkipped() {
         ConfigurationOptions.setSkippedFailsBuild(false);
-        assertThat(skippedStep.getName(), is("<div class=\"skipped\"><span class=\"step-keyword\">And  </span><span class=\"step-name\">the card should be returned</span></div>"
+        assertThat(skippedStep.getName(), is("<div class=\"skipped\"><span class=\"step-keyword\">And  </span><span class=\"step-name\">the card should be returned</span><span class=\"step-duration\">0 ms</span></div>"
         ));
     }
 
     @Test
     public void shouldReturnNameWhenConfigSkippedTurnedOn() {
         ConfigurationOptions.setSkippedFailsBuild(true);
-        assertThat(skippedStep.getName(), is("<div class=\"failed\"><span class=\"step-keyword\">And  </span><span class=\"step-name\">the card should be returned</span><div class=\"step-error-message\"><pre>Mode: Skipped causes Failure<br/><span class=\"skipped\">This step was skipped</span></pre></div></div>"
+        assertThat(skippedStep.getName(), is("<div class=\"failed\"><span class=\"step-keyword\">And  </span><span class=\"step-name\">the card should be returned</span><span class=\"step-duration\">0 ms</span><div class=\"step-error-message\"><pre>Mode: Skipped causes Failure<br/><span class=\"skipped\">This step was skipped</span></pre></div></div>"
         ));
     }
 

commit ca31d87ee7ff2c4e609fe6d4ec9e58b6ebf22446

Handle Scenario Outlines with missing results
Sometimes the json formatter does not include results in step
elements of a Scenario Outline.
For example, see: https://github.com/cucumber/gherkin/issues/165

This caused an NPE when generating reports:

    org.apache.velocity.exception.MethodInvocationException: Invocation of
    method 'getName' in class net.masterthought.cucumber.json.Step threw
    exception java.lang.NullPointerException at
    templates/featureReport.vm[line 210, column 25]

This looks like a regression introduced in 906b9b92a12c5e4c8512b466ca725d2cb74e2653.

src/main/java/net/masterthought/cucumber/json/Step.java
@@ -117,7 +117,7 @@ public class Step {
             content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span><span class=\"step-duration\">" + Util.formatDuration(result.getDuration()) + "</span><div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv() + getImageTags();
         } else if (getStatus() == Util.Status.MISSING) {
             String errorMessage = "<span class=\"missing\">Result was missing for this step</span>";
-            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span><span class=\"step-duration\">" + Util.formatDuration(result.getDuration()) + "</span><div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv();
+            content = Util.result(getStatus()) + "<span class=\"step-keyword\">" + keyword + " </span><span class=\"step-name\">" + name + "</span>" + "<div class=\"step-error-message\"><pre>" + formatError(errorMessage) + "</pre></div>" + Util.closeDiv();
         } else {
             content = getNameAndDuration();
         }