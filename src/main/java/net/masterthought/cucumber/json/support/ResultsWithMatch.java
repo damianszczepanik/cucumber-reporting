9dc94b7e3f30e3aa8f49c9df24ac237219ac1046
Damian Szczepanik <damian.publicemail@gmail.com>
2016-01-12 20:50:59 +0100

    Removed old, unused methods

--- src/main/java/net/masterthought/cucumber/json/support/ResultsWithMatch.java
+++ src/main/java/net/masterthought/cucumber/json/support/ResultsWithMatch.java
@@ -1,6 +1,5 @@
 package net.masterthought.cucumber.json.support;
 
-import net.masterthought.cucumber.json.Embedded;
 import net.masterthought.cucumber.json.Match;
 import net.masterthought.cucumber.json.Result;
 
@@ -15,8 +14,6 @@ public interface ResultsWithMatch {
     public Result getResult();
 
     public Match getMatch();
-
-    public Embedded[] getEmbeddings();
 
     public String getAttachments();
 }
