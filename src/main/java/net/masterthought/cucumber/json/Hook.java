9b6c73f19fce880084d5c25b6e20a9e2df924976
Damian Szczepanik <damian.publicemail@gmail.com>
2016-06-06 23:54:21 +0200

    Embedded images are displayed for hooks #Before and @After
    Issue #439

--- src/main/java/net/masterthought/cucumber/json/Hook.java
+++ src/main/java/net/masterthought/cucumber/json/Hook.java
@@ -8,6 +8,9 @@ public class Hook implements ResultsWithMatch {
     // Start: attributes from JSON file report
     private final Result result = null;
     private final Match match = null;
+
+    // foe Ruby reports
+    private final Embedding[] embeddings = new Embedding[0];
     // End: attributes from JSON file report
 
     private Status status;
@@ -25,6 +28,10 @@ public class Hook implements ResultsWithMatch {
     @Override
     public Match getMatch() {
         return match;
+    }
+
+    public Embedding[] getEmbeddings() {
+        return embeddings;
     }
 
     public void setMedaData() {
