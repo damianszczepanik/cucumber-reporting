3762598124667404fe1a31853b101e5b15f8e9ed
Sam Park <midopa@gmail.com>
2016-06-18 21:35:22 -0700

    Fix IntelliJ compile warning: Charsets deprecated

--- src/test/java/net/masterthought/cucumber/generators/integrations/PageTest.java
+++ src/test/java/net/masterthought/cucumber/generators/integrations/PageTest.java
@@ -1,17 +1,16 @@
 package net.masterthought.cucumber.generators.integrations;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.Charsets;
-import org.apache.commons.lang.StringUtils;
-import org.jsoup.Jsoup;
-import org.junit.After;
-
 import net.masterthought.cucumber.ReportGenerator;
 import net.masterthought.cucumber.ValidationException;
 import net.masterthought.cucumber.generators.AbstractPage;
 import net.masterthought.cucumber.generators.integrations.helpers.DocumentAssertion;
+import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
+import org.junit.After;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Damian Szczepanik (damianszczepanik@github)
@@ -35,7 +34,7 @@ public abstract class PageTest extends ReportGenerator {
     protected DocumentAssertion documentFrom(String pageName) {
         File input = new File(configuration.getReportDirectory(), pageName);
         try {
-            return new DocumentAssertion(Jsoup.parse(input, Charsets.UTF_8.name(), StringUtils.EMPTY));
+            return new DocumentAssertion(Jsoup.parse(input, StandardCharsets.UTF_8.name(), StringUtils.EMPTY));
         } catch (IOException e) {
             throw new ValidationException(e);
         }
