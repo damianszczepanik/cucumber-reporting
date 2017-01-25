3af18503f4380721795b2b0a34bb46b65461a8b6
Damian Szczepanik <damian.publicemail@gmail.com>
2017-01-25 19:04:10 +0100

    Added debug message for Sonar execution to check permissions during pull request

--- sonar.sh
+++ sonar.sh
@@ -3,11 +3,13 @@
 
 if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ];
 then
-  echo for master branch -> upload results to the server
-  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
-    -Dsonar.host.url=https://sonarqube.com -Dsonar.analysis.mode=publish -Dsonar.login=${SONAR_TOKEN};
+  # for master branch -> upload results to the server
+  SONAR_MODE="publish"
 else
-  echo for other branches -> perform only incremental results
-  mvn org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
-    -Dsonar.host.url=https://sonarqube.com -Dsonar.analysis.mode=issues -Dsonar.login=${SONAR_TOKEN}
+  # for other branches -> perform only incremental results
+  SONAR_MODE="issues"
 fi
+
+echo Running Sonar with mode ${SONAR_MODE} for pull request ${TRAVIS_PULL_REQUEST}
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
+  -Dsonar.host.url=https://sonarqube.com -Dsonar.analysis.mode=${SONAR_MODE} -Dsonar.login=${SONAR_TOKEN};
