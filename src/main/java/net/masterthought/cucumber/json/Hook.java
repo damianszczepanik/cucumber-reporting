7b295ddb7ec410fef06daacef5d2ff62903838b1
famrmar <fmarmar@gmail.com>
2017-09-01 12:46:04 +0200

    Issue #656: Include outputs from scenario hooks in reporter

--- src/main/java/net/masterthought/cucumber/json/Hook.java
+++ src/main/java/net/masterthought/cucumber/json/Hook.java
@@ -1,5 +1,9 @@
 package net.masterthought.cucumber.json;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import net.masterthought.cucumber.json.deserializers.OutputsDeserializer;
 import net.masterthought.cucumber.json.support.Resultsable;
 
 public class Hook implements Resultsable {
@@ -8,6 +12,10 @@ public class Hook implements Resultsable {
     private final Result result = null;
     private final Match match = null;
 
+    @JsonDeserialize(using = OutputsDeserializer.class)
+    @JsonProperty("output")
+    private final Output[] outputs = new Output[0];
+    
     // foe Ruby reports
     private final Embedding[] embeddings = new Embedding[0];
     // End: attributes from JSON file report
@@ -21,8 +29,13 @@ public class Hook implements Resultsable {
     public Match getMatch() {
         return match;
     }
+    
+    public Output[] getOutputs() {
+		return outputs;
+	}
 
     public Embedding[] getEmbeddings() {
         return embeddings;
     }
+	
 }
