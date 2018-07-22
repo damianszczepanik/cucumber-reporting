c51f65e77658415fe48817e6c8790254b8465fda
Damian Szczepanik <damianszczepanik@users.noreply.github.com>
2018-07-22 23:15:00 +0200

    Adds tests for uncovered classes (#744)

---
+++ src/test/java/net/masterthought/cucumber/json/MatchTest.java
@@ -0,0 +1,32 @@
+package net.masterthought.cucumber.json;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.masterthought.cucumber.generators.integrations.PageTest;
+
+/**
+ * @author Damian Szczepanik (damianszczepanik@github)
+ */
+public class MatchTest extends PageTest {
+
+    @Before
+    public void setUp() {
+        setUpWithJson(SAMPLE_JSON);
+    }
+
+    @Test
+    public void getLocation_ReturnsLocation() {
+
+        // given
+        Match match = features.get(0).getElements()[1].getSteps()[0].getMatch();
+
+        // when
+        String location = match.getLocation();
+
+        // then
+        assertThat(location).isEqualTo("ATMScenario.createAccount(int)");
+    }
+}
