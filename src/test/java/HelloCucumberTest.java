9b77e395867e361abc3056fd85c1a313c431ef97
Damian Szczepanik <damian.publicemail@gmail.com>
2019-03-15 01:02:52 +0100

    Generate report with 'mvn test'

--- src/test/java/HelloCucumberTest.java
+++ src/test/java/HelloCucumberTest.java
@@ -3,14 +3,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Test;
+
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import net.masterthought.cucumber.presentation.PresentationMode;
 import net.masterthought.cucumber.sorting.SortingMethod;
 
-public class HelloCucumber {
+public class HelloCucumberTest {
 
-    public static void main(String[] args) throws IOException {
+    // test adnotation only to make sure it is generated during "mvn test"
+    @Test
+    public void generateReport() throws IOException {
         File reportOutputDirectory = new File("target/demo");
         List<String> jsonFiles = new ArrayList<>();
         jsonFiles.add("src/test/resources/json/sample.json");
