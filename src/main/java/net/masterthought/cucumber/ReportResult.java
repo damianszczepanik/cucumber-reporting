2c48df123b6c4f33a5f1da1d40162afa959ec9b7
Damian Szczepanik <damianszczepanik@users.noreply.github.com>
2020-05-07 22:08:00 +0200

    Resolve issues reported by sonar (#937)

--- src/main/java/net/masterthought/cucumber/ReportResult.java
+++ src/main/java/net/masterthought/cucumber/ReportResult.java
@@ -3,14 +3,9 @@ package net.masterthought.cucumber;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
-import net.masterthought.cucumber.reducers.ReportFeatureMergerFactory;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.ArrayUtils;
 
 import net.masterthought.cucumber.generators.OverviewReport;
 import net.masterthought.cucumber.json.Element;
@@ -23,8 +18,10 @@ import net.masterthought.cucumber.json.support.Resultsable;
 import net.masterthought.cucumber.json.support.Status;
 import net.masterthought.cucumber.json.support.StepObject;
 import net.masterthought.cucumber.json.support.TagObject;
-import net.masterthought.cucumber.reducers.ReducingMethod;
+import net.masterthought.cucumber.reducers.ReportFeatureMergerFactory;
 import net.masterthought.cucumber.sorting.SortingFactory;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class ReportResult {
 
