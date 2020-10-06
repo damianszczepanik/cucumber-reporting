5d08c2cf3619477c9f15cd663da0064cf5a1753e
FlorianFKaemmerer <Florian.f.kaemmerer@deutschebahn.com>
2020-10-06 10:37:37 +0200

    added javadoc

--- src/main/java/net/masterthought/cucumber/outputhandlers/OutputHandler.java
+++ src/main/java/net/masterthought/cucumber/outputhandlers/OutputHandler.java
@@ -5,9 +5,19 @@ import net.masterthought.cucumber.ValidationException;
 import java.io.File;
 
 /**
+ * OutputHandlers are used to handle the generated Report Files.
+ * for every configured OutputHandler the handle method will be called during the generation process of the report.
+ *
  * @author Florian Kaemmerer (FlorianKaemmerer@github)
  */
 public interface OutputHandler {
 
+    /**
+     * For every configured OutputHandler the handle method will be called during the generation process of the report.
+     *
+     * @param file the file to be handled
+     * @param fileContent content of the file to be handled
+     * @throws ValidationException
+     */
     void handle(File file, byte[] fileContent) throws ValidationException;
 }
