672ffa8bf28d364a352af1bab3d15254cff3f91a
Christian Landvogt <chris@lvgt.de>
2021-08-09 09:07:45 +0200

    Demonstrate custom css/js through syntax highlighting in LiveDemo

--- src/test/java/LiveDemoTest.java
+++ src/test/java/LiveDemoTest.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -37,6 +39,9 @@ public class LiveDemoTest {
         configuration.setQualifier("sample", "Chrome 80, mobile");
         // points to the demo trends which is not used for other tests
         configuration.setTrendsStatsFile(new File("target/test-classes/demo-trends.json"));
+
+        configuration.addCustomCssFiles(Collections.singletonList("src/test/resources/css/stackoverflow-light.min.css"));
+        configuration.addCustomJsFiles(Arrays.asList("src/test/resources/js/enable-highlighting.js", "src/test/resources/js/highlight.min.js"));
 
         ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
         reportBuilder.generateReports();
