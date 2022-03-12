5f64b9da902366adbff465ac50541134d4c250a4
Damian Szczepanik <damian.publicemail@gmail.com>
2022-03-12 23:25:10 +0100

    Prints out exception cause

--- src/main/java/net/masterthought/cucumber/ValidationException.java
+++ src/main/java/net/masterthought/cucumber/ValidationException.java
@@ -2,7 +2,7 @@ package net.masterthought.cucumber;
 
 /**
  * Thrown when report cannot be generated.
- * 
+ *
  * @author Damian Szczepanik (damianszczepanik@github)
  */
 public class ValidationException extends RuntimeException {
@@ -15,7 +15,7 @@ public class ValidationException extends RuntimeException {
         super(message);
     }
 
-    public ValidationException(String message, Exception exception) {
+    public ValidationException(String message, Throwable exception) {
         super(message, exception);
     }
 }
