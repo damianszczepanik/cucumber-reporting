f11e9867941b79e8fc3ba36570018bb6a70ac278
Damian Szczepanik <damianszczepanik@users.noreply.github.com>
2023-08-06 01:08:14 +0200

    Qualifier missing from the report-feature html (#1108)
    Issue #1105

--- src/main/java/net/masterthought/cucumber/generators/FeatureReportPage.java
+++ src/main/java/net/masterthought/cucumber/generators/FeatureReportPage.java
@@ -3,6 +3,7 @@ package net.masterthought.cucumber.generators;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportResult;
 import net.masterthought.cucumber.json.Feature;
+import net.masterthought.cucumber.presentation.PresentationMode;
 
 public class FeatureReportPage extends AbstractPage {
 
@@ -21,6 +22,7 @@ public class FeatureReportPage extends AbstractPage {
     @Override
     public void prepareReport() {
         context.put("feature", feature);
+        context.put("parallel_testing", configuration.containsPresentationMode(PresentationMode.PARALLEL_TESTING));
     }
 
 }
